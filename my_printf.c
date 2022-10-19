390eba9e6211b2e228f333e3da39f8fcf6c4e586
laban254 <labanrotich6544@gmail.com>
2022-10-19 08:44:55 -0700

    my_printf.c

---
+++ my_printf.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - it prints out to console using a specified format
+ *
+ * @format: string that includes the specifiers
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int n;
+	char *parameter_content
+	va_list parameter_list;
+
+	va_start(parameter_list, format);
+	for (n = 0; format[n] != '\0'; n++);
+	{
+		if(format[n] != '%')
+			return(format[n]);
+		else
+		{
+			switch (format[n + 1])
+			{
+				case 'c':
+					parameter_content = va_arg(parameter_list, int);
+					n++;
+					break;
+
+				case 's':
+					parameter_content = va_arg(parameter_list, char);
+					n++;
+					break;
+
+				default:
+					return(format[n]);
+					continue;
+			}
+		}
+	}
+	va_end(parameter_list);
+}
+
+
+
