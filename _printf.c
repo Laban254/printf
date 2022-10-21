70dfd43d4ae836f0e87d2f01313090ddce93f22a
laban254 <labanrotich6544@gmail.com>
2022-10-21 05:46:39 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _printf - function to print anything
+ *
+ * @format: types of argument passed to the function
+ *
+ *  Return: number of characters printed
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list parameter_list;
+	int n;
+
+	va_start(parameter_list, format);
+	done = vfprintf(stdout, format, parameter_list);
+	va_end(parameter_list);
+	return (n);
+}
