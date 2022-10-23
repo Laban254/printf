95ab1494394e885e373fb7722a55b8571df388cb
Evelynmusyoki <kalonduevelyn8@gmail.com>
2022-10-23 13:37:20 -0700

    printf

--- main.h
+++ main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 
-#endif /*MAIN_H*/
+#endif
